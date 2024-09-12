dc56820fbfe01c380ddb2b7a36f86bdd29daadcc
Simão Neto <simaomenezesneto@gmail.com>
2024-09-12 12:34:59 +0100

    feat: Make setting to database, we use h2database

--- src/main/java/com/simaoneto/VendasAppApplication.java
+++ src/main/java/com/simaoneto/VendasAppApplication.java
@@ -12,19 +12,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class VendasAppApplication {
 
-    /*
-        @Autowired
-        @Qualifier("applicationNameFromBean")
-        private String applicationNameFromBean;
-     */
-
-    @Value("${application.name}")
-    private String applicationNameFromProperties;
-
-    @GetMapping("/hello")
-    public String helloWorld(){
-        return applicationNameFromProperties;
-    }
     public static void main(String[] args) {
         SpringApplication.run(VendasAppApplication.class, args);
     }
