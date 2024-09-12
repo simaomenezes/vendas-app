2aec8a58de0b6ed4d4d8effa5226af7ab9ab8e4b
Simão Neto <simaomenezesneto@gmail.com>
2024-09-12 19:36:12 +0100

    feat: Create entities and operations fluxo to CRUD

---
+++ src/main/java/com/simaoneto/domain/entity/ItemPedido.java
@@ -0,0 +1,41 @@
+package com.simaoneto.domain.entity;
+
+public class ItemPedido {
+
+    private Integer id;
+    private Produto produto;
+    private Pedido pedido;
+    private Integer quantidade;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Produto getProduto() {
+        return produto;
+    }
+
+    public void setProduto(Produto produto) {
+        this.produto = produto;
+    }
+
+    public Pedido getPedido() {
+        return pedido;
+    }
+
+    public void setPedido(Pedido pedido) {
+        this.pedido = pedido;
+    }
+
+    public Integer getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(Integer quantidade) {
+        this.quantidade = quantidade;
+    }
+}
