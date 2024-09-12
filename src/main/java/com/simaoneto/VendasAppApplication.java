4c234d340f3c3382d0ec0ba49ed8116cbf84f64a
Simão Neto <simaomenezesneto@gmail.com>
2024-09-12 20:30:46 +0100

    feat: Working with JPA

--- src/main/java/com/simaoneto/VendasAppApplication.java
+++ src/main/java/com/simaoneto/VendasAppApplication.java
@@ -3,13 +3,13 @@ package com.simaoneto;
 import com.simaoneto.domain.entity.Cliente;
 import com.simaoneto.repository.Clientes;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.beans.factory.annotation.Value;
+
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.bind.annotation.GetMapping;
+
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
