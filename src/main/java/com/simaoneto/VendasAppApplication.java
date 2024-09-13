90c8400082b25add5304e126572d29e72ef4c606
Simão Neto <simaomenezesneto@gmail.com>
2024-09-13 17:16:16 +0100

    feat: Criando nosso primeiro controller

--- src/main/java/com/simaoneto/VendasAppApplication.java
+++ src/main/java/com/simaoneto/VendasAppApplication.java
@@ -1,36 +1,16 @@
 package com.simaoneto;
 
-import com.simaoneto.domain.entity.Cliente;
-
-import com.simaoneto.domain.repository.Clientes;
-import org.springframework.beans.factory.annotation.Autowired;
 
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
+
 
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @SpringBootApplication
 @RestController
 public class VendasAppApplication {
-
-    @Bean
-    public CommandLineRunner init(@Autowired Clientes clientes){
-        return args -> {
-            System.out.println("Salvando clientes");
-            clientes.save(new Cliente("Fulano"));
-            clientes.save(new Cliente("Outro Cliente"));
-
-            boolean existe = clientes.existsByNome("Neto");
-            System.out.println("existe um cliente com o nome Neto? " + existe);
-        };
-    }
-
     public static void main(String[] args) {
         SpringApplication.run(VendasAppApplication.class, args);
     }
