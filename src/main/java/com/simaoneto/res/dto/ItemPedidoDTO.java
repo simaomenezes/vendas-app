f79bf3e1cfaec73a868d820718478890baa36373
Simão Neto <simaomenezesneto@gmail.com>
2024-09-14 15:37:55 +0100

    feat: Criando classes DTOs para auxiliar no fluxo das informações

--- src/main/java/com/simaoneto/res/dto/ItemPedidoDTO.java
+++ src/main/java/com/simaoneto/res/dto/ItemPedidoDTO.java
@@ -1,4 +1,13 @@
 package com.simaoneto.res.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ItemPedidoDTO {
+    private Integer produto;
+    private Integer quantidade;
 }
