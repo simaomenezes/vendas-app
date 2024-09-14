c4c42737f82b43f776615b199d93132805f6b4a6
Simão Neto <simaomenezesneto@gmail.com>
2024-09-14 15:38:37 +0100

    feat: Criando classes para fluxo de pedido.

--- src/main/java/com/simaoneto/service/PedidoService.java
+++ src/main/java/com/simaoneto/service/PedidoService.java
@@ -1,4 +1,8 @@
 package com.simaoneto.service;
 
+import com.simaoneto.domain.entity.Pedido;
+import com.simaoneto.res.dto.PedidoDTO;
+
 public interface PedidoService {
+    Pedido salvar(PedidoDTO pedidoDTO);
 }
--- src/main/java/com/simaoneto/service/impl/PedidoServiceImpl.java
+++ src/main/java/com/simaoneto/service/impl/PedidoServiceImpl.java
@@ -1,4 +1,22 @@
 package com.simaoneto.service.impl;
 
-public class PedidoServiceImpl {
+import com.simaoneto.domain.entity.Pedido;
+import com.simaoneto.domain.repository.Pedidos;
+import com.simaoneto.res.dto.PedidoDTO;
+import com.simaoneto.service.PedidoService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PedidoServiceImpl implements PedidoService {
+
+    private Pedidos pedidos;
+
+    public PedidoServiceImpl(Pedidos pedidos) {
+        this.pedidos = pedidos;
+    }
+
+    @Override
+    public Pedido salvar(PedidoDTO pedidoDTO) {
+        return null;
+    }
 }
