2f255877904240be3f5965aa2ef8a020283628d8
Simão Neto <simaomenezesneto@gmail.com>
2024-09-14 15:38:37 +0100

    feat: Criando classes para fluxo de pedido.

--- src/main/java/com/simaoneto/res/controller/PedidoController.java
+++ src/main/java/com/simaoneto/res/controller/PedidoController.java
@@ -1,4 +1,16 @@
 package com.simaoneto.res.controller;
 
+import com.simaoneto.service.PedidoService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/pedidos/")
 public class PedidoController {
+
+    private PedidoService pedidoService;
+
+    public PedidoController(PedidoService pedidoService) {
+        this.pedidoService = pedidoService;
+    }
 }
