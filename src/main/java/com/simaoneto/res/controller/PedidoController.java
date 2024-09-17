07afb9ea5ae478ce4d2ca8df0d310df5847ed34c
Simão Neto <simaomenezesneto@gmail.com>
2024-09-17 15:30:38 +0100

    feat: Criando fluxo para salvar pedido.

--- src/main/java/com/simaoneto/res/controller/PedidoController.java
+++ src/main/java/com/simaoneto/res/controller/PedidoController.java
@@ -1,8 +1,10 @@
 package com.simaoneto.res.controller;
 
+import com.simaoneto.domain.entity.Pedido;
+import com.simaoneto.res.dto.PedidoDTO;
 import com.simaoneto.service.PedidoService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/pedidos/")
@@ -12,5 +14,12 @@ public class PedidoController {
 
     public PedidoController(PedidoService pedidoService) {
         this.pedidoService = pedidoService;
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    public Integer salvar(@RequestBody PedidoDTO pedidoDTO){
+        Pedido pedido = pedidoService.salvar(pedidoDTO);
+        return pedido.getId();
     }
 }
