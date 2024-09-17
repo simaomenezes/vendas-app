3b3c0bb1dcb640aad7374da62cfbb450bdc380f7
Simão Neto <simaomenezesneto@gmail.com>
2024-09-17 17:39:32 +0100

    adicionando novo atributo ao cliente, adicionando spring-boot-devtools ao projeto e adicionando @ExceptionHandler ao método handleRegraNegocioException

--- src/main/java/com/simaoneto/domain/entity/Cliente.java
+++ src/main/java/com/simaoneto/domain/entity/Cliente.java
@@ -21,6 +21,9 @@ public class Cliente {
     @Column(name = "nome", length = 100)
     private String nome;
 
+    @Column(name = "cpf", length = 11)
+    private String cpf;
+
     @OneToMany(mappedBy = "cliente", fetch = FetchType.LAZY)
     private Set<Pedido> pedidos;
 }
