7175b7e2465cd6c61f931c9a7e854052a57bdd34
Simão Neto <simaomenezesneto@gmail.com>
2024-09-17 23:52:28 +0100

    feat: Realizando o fluxo de pesquisa por id e atualização de estatus

--- src/main/java/com/simaoneto/res/controller/PedidoController.java
+++ src/main/java/com/simaoneto/res/controller/PedidoController.java
@@ -1,10 +1,25 @@
 package com.simaoneto.res.controller;
 
+import com.simaoneto.domain.entity.ItemPedido;
 import com.simaoneto.domain.entity.Pedido;
+import com.simaoneto.domain.enums.StatusPedido;
+import com.simaoneto.res.dto.AtualizacaoStatusPedidoDTO;
+import com.simaoneto.res.dto.InformacaoItemPedidoDTO;
+import com.simaoneto.res.dto.InformacoesPedidoDTO;
 import com.simaoneto.res.dto.PedidoDTO;
 import com.simaoneto.service.PedidoService;
 import org.springframework.http.HttpStatus;
+import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.time.format.DateTimeFormatter;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.springframework.http.HttpStatus.NOT_FOUND;
+import static org.springframework.http.HttpStatus.NO_CONTENT;
 
 @RestController
 @RequestMapping("/api/pedidos/")
@@ -21,5 +36,43 @@ public class PedidoController {
     public Integer salvar(@RequestBody PedidoDTO pedidoDTO){
         Pedido pedido = pedidoService.salvar(pedidoDTO);
         return pedido.getId();
+    }
+
+    @GetMapping("{id}")
+    public InformacoesPedidoDTO getById(@PathVariable Integer id){
+        return pedidoService.obterPedidoCompleto(id).map(this::converter).orElseThrow(() -> new ResponseStatusException(NOT_FOUND, "Pedido não encontrado."));
+    }
+
+    @PatchMapping("{id}")
+    @ResponseStatus(NO_CONTENT)
+    public void updateStatus(@PathVariable Integer id, @RequestBody AtualizacaoStatusPedidoDTO dto){
+        String novoStatus = dto.getNovoStatus();
+        pedidoService.atualizaStatus(id, StatusPedido.valueOf(novoStatus));
+    }
+
+    private InformacoesPedidoDTO converter(Pedido pedido){
+        return InformacoesPedidoDTO
+                .builder()
+                .codigo(pedido.getId())
+                .dataPedido(pedido.getDataPedido().format(DateTimeFormatter.ofPattern("dd/MM/yyyy")))
+                .cpf(pedido.getCliente().getCpf())
+                .nomeCliente(pedido.getCliente().getNome())
+                .total(pedido.getTotal())
+                .status(pedido.getStatus().name())
+                .items(converter(pedido.getItens()))
+                .build();
+    }
+
+    private List<InformacaoItemPedidoDTO> converter(List<ItemPedido> itens){
+        if(CollectionUtils.isEmpty(itens)){
+            return Collections.emptyList();
+        }
+        return itens.stream().map(
+                item -> InformacaoItemPedidoDTO
+                        .builder().descricaoProduto(item.getProduto().getDescricao())
+                        .precoUnitario(item.getProduto().getPreco())
+                        .quantidade(item.getQuantidade())
+                        .build()
+        ).collect(Collectors.toList());
     }
 }
