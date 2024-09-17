c3dedcabf622feffc22f6c23c07795141e97de00
Simão Neto <simaomenezesneto@gmail.com>
2024-09-17 17:59:11 +0100

    feat: Criando class para controller das exceptions referente ao pedido.

--- src/main/java/com/simaoneto/res/controller/ApplicationControllerAdvice.java
+++ src/main/java/com/simaoneto/res/controller/ApplicationControllerAdvice.java
@@ -1,5 +1,6 @@
 package com.simaoneto.res.controller;
 
+import com.simaoneto.exception.PedidoNaoEncontradoException;
 import com.simaoneto.exception.RegraNegocioException;
 import com.simaoneto.rest.ApiErrors;
 import org.springframework.http.HttpStatus;
@@ -15,5 +16,11 @@ public class ApplicationControllerAdvice {
     public ApiErrors handleRegraNegocioException(RegraNegocioException exception){
         String mensageError = exception.getMessage();
         return new ApiErrors(mensageError);
+    }
+
+    @ExceptionHandler(PedidoNaoEncontradoException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ApiErrors handlePedidosNotFoundException(PedidoNaoEncontradoException ex){
+        return new ApiErrors(ex.getMessage());
     }
 }
