7e427a77123b9f6d096f46d6eec0c3f682105a2f
Simão Neto <simaomenezesneto@gmail.com>
2024-09-17 17:39:32 +0100

    adicionando novo atributo ao cliente, adicionando spring-boot-devtools ao projeto e adicionando @ExceptionHandler ao método handleRegraNegocioException

--- src/main/java/com/simaoneto/res/controller/ApplicationControllerAdvice.java
+++ src/main/java/com/simaoneto/res/controller/ApplicationControllerAdvice.java
@@ -2,11 +2,16 @@ package com.simaoneto.res.controller;
 
 import com.simaoneto.exception.RegraNegocioException;
 import com.simaoneto.rest.ApiErrors;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class ApplicationControllerAdvice {
 
+    @ExceptionHandler(RegraNegocioException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ApiErrors handleRegraNegocioException(RegraNegocioException exception){
         String mensageError = exception.getMessage();
         return new ApiErrors(mensageError);
