0b97887d6e579cc8ba9c5fefce7c5e842be6eb39
Simão Neto <simaomenezesneto@gmail.com>
2024-09-18 20:50:22 +0100

    feat: Adicionando rHttpBasicConfigurer ao nosso SecurityConfig

--- src/main/java/com/simaoneto/config/SecurityConfig.java
+++ src/main/java/com/simaoneto/config/SecurityConfig.java
@@ -37,7 +37,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("api/produtos/**")
                 .hasAnyRole("ADMIN")
         .and()
-                .formLogin();
+                .httpBasic();
 
     }
 }
