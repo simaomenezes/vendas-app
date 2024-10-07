cc546b7f9b04822f74668a6cadccc8a28784dbfb
Simão Neto <simaomenezesneto@gmail.com>
2024-10-07 21:38:46 +0100

    Chore: Maked class to flow JWT

---
+++ src/main/java/com/simaoneto/security/jwt/JwtService.java
@@ -0,0 +1,61 @@
+package com.simaoneto.security.jwt;
+
+import com.simaoneto.domain.entity.Usuario;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Date;
+
+
+
+@Service
+public class JwtService {
+
+    @Value("${security.jwt.expiracao}")
+    private String expiracao;
+
+    @Value("${security.jwt.chave-assinatura}")
+    private String chaveassinatura;
+
+    public String gerarToken(Usuario usuario){
+        long expString = Long.valueOf(expiracao);
+        LocalDateTime dataHoraExpiracao = LocalDateTime.now().plusMinutes(expString);
+        Instant instance = dataHoraExpiracao.atZone(ZoneId.systemDefault()).toInstant();
+        Date date = Date.from(instance);
+
+        return Jwts.builder().setSubject(usuario.getLogin())
+                .setExpiration(date)
+                .signWith(SignatureAlgorithm.ES512, chaveassinatura)
+                .compact();
+    }
+
+    private Claims obterClaims(String token){
+        return Jwts
+                .parser()
+                .setSigningKey(chaveassinatura)
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    public boolean tokenValido(String token){
+        try {
+            Claims claims = obterClaims(token);
+            Date expDate = claims.getExpiration();
+            LocalDateTime data = expDate.toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime();
+            return !LocalDateTime.now().isAfter(data);
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public String obterLoginUsuario(String token){
+       return obterClaims(token).getSubject();
+    }
+}
