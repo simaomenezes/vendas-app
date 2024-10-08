391579215fb390c0b1f076ca3ec11ca4305084fa
Simão Neto <simaomenezesneto@gmail.com>
2024-10-08 12:38:19 +0100

    refact: Add GenerationType.IDENTITY to entities for mysql

--- src/main/java/com/simaoneto/domain/entity/Cliente.java
+++ src/main/java/com/simaoneto/domain/entity/Cliente.java
@@ -16,7 +16,7 @@ import java.util.Set;
 @Table(name = "cliente")
 public class Cliente {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private Integer id;
 
