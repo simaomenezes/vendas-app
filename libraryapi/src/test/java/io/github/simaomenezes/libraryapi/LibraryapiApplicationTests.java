f6582b7b8d1a753892c5492dfa1bcd516617f234
Simão Neto <simaomenezesneto@gmail.com>
2024-10-23 13:49:18 +0100

    chore: Add new projetct - GO LEARNING - spring data jpa

---
+++ libraryapi/src/test/java/io/github/simaomenezes/libraryapi/LibraryapiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.simaomenezes.libraryapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
