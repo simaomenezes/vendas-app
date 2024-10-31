fbedeb622e79f04f3d998dc7703841d9bb35a16e
Simão Neto <simaomenezesneto@gmail.com>
2024-10-31 14:25:13 +0000

    feat: Create model for application

---
+++ libraryapi/src/main/java/io/github/simaomenezes/libraryapi/model/Book.java
@@ -0,0 +1,38 @@
+package io.github.simaomenezes.libraryapi.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Entity
+@Table(name = "book")
+@Data
+public class Book {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "isbn", length = 20, nullable = false)
+    private String isbn;
+
+    @Column(name = "title", length = 150, nullable = false)
+    private String title;
+
+    @Column(name = "date_published")
+    private LocalDate datePublished;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "gender", length = 30, nullable = false)
+    private BookGender gender;
+
+    @Column(name = "price", precision = 18, scale = 2)
+    private BigDecimal price;
+
+    @ManyToOne
+    @JoinColumn(name = "id_author")
+    private Author author;
+}
