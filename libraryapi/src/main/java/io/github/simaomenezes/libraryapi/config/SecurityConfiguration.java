9fb1eb129177db5b7a87bb87854569c3446ae48b
Simão Neto <simaomenezesneto@gmail.com>
2024-12-17 12:30:34 +0000

    feat: Create auth basic.

---
+++ libraryapi/src/main/java/io/github/simaomenezes/libraryapi/config/SecurityConfiguration.java
@@ -0,0 +1,23 @@
+package io.github.simaomenezes.libraryapi.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+    public SecurityFilterChain securityFilterChainHttp(HttpSecurity http) throws Exception {
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .httpBasic(Customizer.withDefaults())
+                .authorizeHttpRequests(authorize -> {
+                    authorize.anyRequest().authenticated();
+                })
+                .build();
+    }
+}
