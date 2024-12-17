539eaffca6f6a41acc093e29e78924de82bacd02
Simão Neto <simaomenezesneto@gmail.com>
2024-12-17 14:27:55 +0000

    feat: Create structure of users

--- libraryapi/src/main/java/io/github/simaomenezes/libraryapi/config/SecurityConfiguration.java
+++ libraryapi/src/main/java/io/github/simaomenezes/libraryapi/config/SecurityConfiguration.java
@@ -1,10 +1,17 @@
 package io.github.simaomenezes.libraryapi.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -15,12 +22,39 @@ public class SecurityConfiguration {
         return http
                 .csrf(AbstractHttpConfigurer::disable)
                 .httpBasic(Customizer.withDefaults())
-                .formLogin(configurer->{
-                    configurer.loginPage("/login").permitAll();
+                .formLogin(configured->{
+                    configured.loginPage("/login").permitAll();
                 })
                 .authorizeHttpRequests(authorize -> {
+                    authorize.requestMatchers("/login/**").permitAll();
+                    authorize.requestMatchers("/authors/**").hasRole("ADMIN");
+                    authorize.requestMatchers("/books/**").hasAnyRole("USER","ADMIN");
+
                     authorize.anyRequest().authenticated();
                 })
                 .build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder(10);
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(PasswordEncoder passwordEncoder){
+        UserDetails userDetails1 = User.builder()
+                .username("user")
+                .password(passwordEncoder.encode("123"))
+                .roles("USER")
+                .build();
+
+
+        UserDetails userDetails2 = User.builder()
+                .username("admin")
+                .password(passwordEncoder.encode("123"))
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(userDetails1, userDetails2);
     }
 }
