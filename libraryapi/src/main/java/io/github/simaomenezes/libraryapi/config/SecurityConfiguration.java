6a604a432353f53b1dc19e9a52899d48eadcddf7
Simão Neto <simaomenezesneto@gmail.com>
2024-12-19 14:04:07 +0000

    refact: create structure user

--- libraryapi/src/main/java/io/github/simaomenezes/libraryapi/config/SecurityConfiguration.java
+++ libraryapi/src/main/java/io/github/simaomenezes/libraryapi/config/SecurityConfiguration.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -19,6 +20,7 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity(securedEnabled = true, jsr250Enabled = true)
 public class SecurityConfiguration {
 
     public SecurityFilterChain securityFilterChainHttp(HttpSecurity http) throws Exception {
