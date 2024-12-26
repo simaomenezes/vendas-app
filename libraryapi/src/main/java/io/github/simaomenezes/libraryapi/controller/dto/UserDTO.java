e06f2141b35bf67713318cb6a53be795708d7da4
Simão Neto <simaomenezesneto@gmail.com>
2024-12-26 15:53:45 +0000

    feat: Add new field to User our model, add new methods

--- libraryapi/src/main/java/io/github/simaomenezes/libraryapi/controller/dto/UserDTO.java
+++ libraryapi/src/main/java/io/github/simaomenezes/libraryapi/controller/dto/UserDTO.java
@@ -2,4 +2,4 @@ package io.github.simaomenezes.libraryapi.controller.dto;
 
 import java.util.List;
 
-public record  UserDTO(String login, String password, List<String> roles){}
+public record  UserDTO(String login, String password, String email, List<String> roles){}
---
+++ libraryapi/src/main/java/io/github/simaomenezes/libraryapi/security/CustomAuthentication.java
@@ -0,0 +1,4 @@
+package io.github.simaomenezes.libraryapi.security;
+
+public class CustomAuthentication {
+}
