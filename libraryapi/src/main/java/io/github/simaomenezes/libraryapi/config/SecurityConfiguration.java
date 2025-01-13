d608824c273fa47a9c37d61edd88203f2a789de6
Simão Neto <simaomenezesneto@gmail.com>
2025-01-13 10:55:23 +0000

    feat: Add doc about OpenAPI

--- libraryapi/src/main/java/io/github/simaomenezes/libraryapi/config/SecurityConfiguration.java
+++ libraryapi/src/main/java/io/github/simaomenezes/libraryapi/config/SecurityConfiguration.java
@@ -12,6 +12,7 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.core.GrantedAuthorityDefaults;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -34,7 +35,7 @@ public class SecurityConfiguration {
             JwtAuthenticationCustomFilter jwtAuthenticationCustomFilter) throws Exception {
         return http
                 .csrf(AbstractHttpConfigurer::disable)
-                .httpBasic(Customizer.withDefaults())
+               // .httpBasic(Customizer.withDefaults())
                 .formLogin(configured->{
                     configured
                             .loginPage("/login");
@@ -85,5 +86,17 @@ public class SecurityConfiguration {
         var converter = new JwtAuthenticationConverter();
         converter.setJwtGrantedAuthoritiesConverter(jwtGrantedAuthoritiesConverter);
         return converter;
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer(){
+        return web -> web.ignoring().requestMatchers(
+                "/v2/api-docs/**",
+                "/v3/api-docs/**",
+                "/swagger-resources/**",
+                "/swagger-ui.html",
+                "/swagger-ui/**",
+                "/webjars/**"
+        );
     }
 }
